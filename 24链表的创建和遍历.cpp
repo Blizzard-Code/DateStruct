a8243d08ad5c127cf5b733739a4a88a0e2c75d70
pro <849826920@qq.com>
2021-08-24 11:54:48 +0800

    修改

--- 24链表的创建和遍历.cpp
+++ 24链表的创建和遍历.cpp
@@ -27,14 +27,14 @@ PNODE create_list()
 	int val;//用来临时存放用户输入的节点值
 
 	//分配了一个不存放有效数据的头结点
-	PNODE pHead = (PNODE)malloc(sizeof(NODE));//头结点
+	PNODE pHead = (PNODE)malloc(sizeof(NODE));//头结点①
 	if (pHead == NULL)
 	{
 		printf("分配失败，程序终止\n");
 		exit(-1);
 	}
-	PNODE pTail = pHead;
-	pTail->pNext = NULL;
+	PNODE pTail = pHead;//pTail和pHEAD都指向头结点 ②
+	pTail->pNext = NULL;//头结点的指针域情空③
 
 	printf("请输入需要生存链表节点的个数：len = " );
 	scanf_s("%d", &len);
@@ -43,16 +43,16 @@ PNODE create_list()
 	{
 		printf("请输入%d个节点的值", i + 1);
 		scanf_s("%d", &val);
-		PNODE pNew = (PNODE)malloc(sizeof(NODE));
+		PNODE pNew = (PNODE)malloc(sizeof(NODE));//④
 		if (pNew == NULL)  
 		{  
 			printf("分配失败，程序终止\n");
 			exit(-1);
 		}
-		pNew->data = val; //生成临时节点
-		pTail->pNext = pNew;
-		pNew->pNext = NULL;
-		pTail = pNew;
+		pNew->data = val; //生成临时节点⑤
+		pTail->pNext = pNew; //指向新造的pNew的节点，pNew放在pTail的后面 ⑥
+		pNew->pNext = NULL;//新造的节点的指针域为空⑦
+		pTail = pNew;//Ptail永远指向指向尾节点，⑧
 	}
 	printf("------------------------------\n");
 
@@ -62,9 +62,9 @@ PNODE create_list()
 
 void traverse_list(PNODE pHead)
 {
-	PNODE p = pHead->pNext;
+	PNODE p = pHead->pNext;//p指向第一个有效节点
 	
-	while (NULL != p)
+	while (NULL != p)//判断p指向的节点的
 	{
 		int i = 1;
 		printf("第%d个节点的数据是%d\n",i, p->data);
